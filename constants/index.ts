42895a3525df14824083fad602594fcaa088a427
huytamlee <huytamlee@gmail.com>
2025-04-11 10:46:16 +0700

    MobileNav and NavLinks

---
+++ constants/index.ts
@@ -0,0 +1,37 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    label: "Home",
+    route: "/",
+  },
+  {
+    imgURL: "/icons/users.svg",
+    label: "Community",
+    route: "/community",
+  },
+  {
+    imgURL: "/icons/star.svg",
+    label: "Collections",
+    route: "/collection",
+  },
+  {
+    imgURL: "/icons/suitcase.svg",
+    label: "Find Jobs",
+    route: "/jobs",
+  },
+  {
+    imgURL: "/icons/tag.svg",
+    route: "/tags",
+    label: "Tags",
+  },
+  {
+    imgURL: "/icons/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+  {
+    imgURL: "/icons/question.svg",
+    route: "/ask-question",
+    label: "Ask a question",
+  },
+];
